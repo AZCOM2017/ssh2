19cd0a2f07f344b2c6941dff1c0768f1280170a2
chengrow <2683607221@qq.com>
2018-11-02 11:09:13 +0800

    Spring JDBC ,NamedParameterJdbcTemplate类的使用

---
+++ spring8x1/src/main/java/cap/dao/AdminDAO.java
@@ -0,0 +1,19 @@
+package cap.dao;
+
+import cap.bean.Admin;
+
+import java.util.List;
+
+public interface AdminDAO {
+
+    List<Admin> findAdmins();
+
+    Admin findById(int id);
+
+    int updateAdmin(Admin admin);
+
+    int delAdmin(int id);
+
+    int addAdmin(Admin admin);
+
+}
